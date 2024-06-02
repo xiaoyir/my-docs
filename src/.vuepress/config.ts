7a18ab88a5ea26769db0bc1cafa8178029c829f0
whuhbz <463436681@qq.com>
2024-06-02 17:45:46 +0800

    修改导航和目录

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,8 +8,8 @@ export default defineUserConfig({
   base: "/my-docs/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "Java库",
+  description: "个人博客",
 
   theme,
 
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,24 +3,112 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
+    // {
+    //   text: "如何使用",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
+    // {
+    //   text: "文章",
+    //   icon: "book",
+    //   prefix: "posts/",
+    //   children: "structure",
+    // },
+    // "intro",
+    // {
+    //   text: "幻灯片",
+    //   icon: "person-chalkboard",
+    //   link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+    // },
     {
-      text: "如何使用",
+      text: "Java",
       icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
+      prefix: "skill/java",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
     },
     {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
+      text: "JVM",
+      icon: "laptop-code",
+      prefix: "skill/jvm",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
     },
-    "intro",
     {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+      text: "github",
+      icon: "laptop-code",
+      prefix: "skill/github",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "nginx",
+      icon: "laptop-code",
+      prefix: "skill/nginx",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "mysql",
+      icon: "laptop-code",
+      prefix: "skill/mysql",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "redis",
+      icon: "laptop-code",
+      prefix: "skill/redis",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "sentinel",
+      icon: "laptop-code",
+      prefix: "skill/sentinel",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "canal",
+      icon: "laptop-code",
+      prefix: "skill/canal",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "spring",
+      icon: "laptop-code",
+      prefix: "skill/spring",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "其他",
+      icon: "laptop-code",
+      prefix: "skill/other",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
+    },
+    {
+      text: "学习书籍",
+      icon: "laptop-code",
+      prefix: "skill/books",
+      collapsible: true,
+      expanded: false,
+      children: "structure"
     },
   ],
 });
